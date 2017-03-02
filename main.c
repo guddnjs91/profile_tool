c0e6e22ca533c0891dada1c7987ffdbed0df6f54
Hyeongwon Jang <guddnjs91@gmail.com>
2017-03-03 06:19:53 +0900

    Add function pointer in op_obj structure

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int funcA(int, int);
 int funcB(int, int);
+void* (*funcptr)(void*, void*);
 
 int main(int argc, char* argv[]) {
 
@@ -9,16 +10,22 @@ int main(int argc, char* argv[]) {
     int retA, retB;
     struct op_obj opA, opB;
     
-    init_op_obj(&opA);
-    init_op_obj(&opB);
+    init_op_obj(&opA, funcA);
+    init_op_obj(&opB, funcB);
     
     clock_gettime(CLOCK_MONOTONIC, &opA.start);
-    retA = funcA(a, b);
+    // "( ret (*) args ) func_addr" => represents function name
+    funcptr = (void* (*) (void*, void*)) funcA;
+    //retA = (int) funcptr(a, b);
+    retA = opA.func(a, b);
+    printf("%d\n", retA);
     clock_gettime(CLOCK_MONOTONIC, &opA.end);
     calc_time(&opA);
 
     clock_gettime(CLOCK_MONOTONIC, &opB.start);
-    retB = funcB(a, b);
+    //retB = funcB(a, b);
+    retB = opB.func(a, b);
+    printf("%d\n", retB);
     clock_gettime(CLOCK_MONOTONIC, &opB.end);
     calc_time(&opB);
 
--- ut0profile.h
+++ ut0profile.h
@@ -1,31 +1,32 @@
 #ifndef __PROFILE_H__
 #define __PROFILE_H__
+
+#define __STDC_FORMAT_MACROS
 #include <time.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 #define LATENCY_RANGE_CNT   (20)
 #define BILLION             (1000000000L)
-
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
 
 uint32_t range_endpoint[LATENCY_RANGE_CNT - 1] = { 100, 500, // ns
     1000, 50000, 100000, 200000, 300000, 400000,
     500000, 600000, 700000, 800000, 900000, // us
     1000000, 5000000, 10000000, 20000000, 50000000, 100000000 }; // ms
-uint32_t global_counter; // unique id provider
 
 struct op_obj {
-    uint32_t oid; // operation id
     struct timespec start;
     struct timespec end;
     uint64_t range[LATENCY_RANGE_CNT];
+    int (*func)(int, int); // function pointer TODO: generalize this
 };
 
-void init_op_obj(struct op_obj* op) {    
-    op->oid = __sync_fetch_and_add(&global_counter, 1);
+void init_op_obj(struct op_obj* op, void *func) {
+
     memset(&(op->range), 0, sizeof(uint64_t) * LATENCY_RANGE_CNT);
+    op->func = func;
 }
 
 void calc_time(struct op_obj* op) {
@@ -51,7 +52,7 @@ void calc_time(struct op_obj* op) {
 void print_latency_distr(struct op_obj* op) {
     int i;
     for (i = 0; i < LATENCY_RANGE_CNT - 1 ; i++) {
-        printf(" %4u%s ~ %4u%s: %llu\n", 
+        printf(" %4u%s ~ %4u%s: %"PRIu64"\n", 
                 i == 0 ? 0 : 
                  (range_endpoint[i-1] >= 1000000 ? range_endpoint[i-1]/1000000 :
                   (range_endpoint[i-1] >= 1000 ? range_endpoint[i-1]/1000 : range_endpoint[i-1])),
@@ -61,7 +62,7 @@ void print_latency_distr(struct op_obj* op) {
                 range_endpoint[i] >= 1000000 ? "ms" : (range_endpoint[i] >= 1000 ? "us" : "ns"),
                 op->range[i]); 
     }
-    printf(" %4u%s ~       : %llu\n", range_endpoint[i], "ms", op->range[i]);
+    printf(" %4u%s ~       : %"PRIu64"\n", range_endpoint[i], "ms", op->range[i]);
 }
 
 #endif
